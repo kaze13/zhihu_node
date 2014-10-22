5f65ac483f7dc6cf3d94f36d86da80f831ea5acc
Minli Chen <cml_hawke0@163.com>
2014-10-22 22:18:05 +0800

    ahjaha

--- public/js/app.js
+++ public/js/app.js
@@ -7,29 +7,35 @@ var mainApp = angular.module('mainApp', []);
 mainApp.controller('indexCtrl', function($scope, $http) {
   var self_ = this;
   this.currentPage_  = 1;
+  $scope.currentAnswers = {ask_title:'asd'};
   $http.get('/get/answers/1/20').
-    success(function(answers, status, headers, config) {
+    success(function(answers) {
       $scope.answers = answers;
     });
 
-  $scope.showAnswer = function(){
+  $scope.showAnswer = function(id){
+	$http.get('/get/answer/' + id).success(function(answer){
+		$scope.currentAnswer = answer;
+	})
 
   };
 
-  $('#prev-link').click(function(){
-    self_.currentPage_ = self_.currentPage_ - 1;
-    $http.get('/get/answers/'+self_.currentPage_+'/20').
-      success(function(answers, status, headers, config) {
-        $scope.answers = answers;
-      });
-  });
+	
 
-  $('#next-link').click(function(){
-    self_.currentPage_ = self_.currentPage_ + 1;
+  $scope.prevPage = function(){
+    $scope.currentPage_ = $scope.currentPage_ - 1;
     $http.get('/get/answers/'+self_.currentPage_+'/20').
       success(function(answers, status, headers, config) {
         $scope.answers = answers;
       });
-  });
+  };
+
+  $scope.nextPage = function(){
+    $scope.currentPage_ = $scope.currentPage_ + 1;
+    $http.get('/get/answers/'+self_.currentPage_+'/20').
+      success(function(answers, status, headers, config) {
+        $scope.answers = answers;
+      });
+  };
 
 });
